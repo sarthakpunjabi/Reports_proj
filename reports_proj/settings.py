a4396022001efa8773ff05e27bbc0a74ca36f159
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-03-09 11:29:57 +0000

    Changing the configuration for media , static and templates and adding application to installed application

--- reports_proj/settings.py
+++ reports_proj/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Our Apps
+    'customers',
+    'products',
+    'profiles',
+    'reports',
+    'sales',
+
+    #3rd Party
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +64,7 @@ ROOT_URLCONF = 'reports_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,10 +122,18 @@ USE_I18N = True
 USE_TZ = True
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
